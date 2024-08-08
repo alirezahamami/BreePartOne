eafb389fdff4166ec7dd7e35b5c92d7174b78d52
Alireza <alireza.hamami@Gmail.com>
2024-08-08 11:09:04 -0300

    add deployment port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.static(__dirname + "/Bree-Tech-FrontEnd-P1/dist"));
